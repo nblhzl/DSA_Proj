3bef9cf703bd7d6d29c9ce285c59e7d2d9531ec1
GGohYL <sitgohyl@gmail.com>
2024-06-27 11:59:52 +0800

    commented bellman

--- walk_drive_bike.py
+++ walk_drive_bike.py
@@ -102,8 +102,8 @@ a_star_cost, a_star_path = custom_a_star(G, start_node, end_node)
 print(f"Shortest path using A* algorithm from node {start_node} to node {end_node} has cost {a_star_cost} and path {a_star_path}")
 
 # Find shortest path using custom Bellman-Ford algorithm
-bellman_ford_cost, bellman_ford_path = custom_bellman_ford(G, start_node, end_node)
-print(f"Shortest path using Bellman-Ford algorithm from node {start_node} to node {end_node} has cost {bellman_ford_cost} and path {bellman_ford_path}")
+# bellman_ford_cost, bellman_ford_path = custom_bellman_ford(G, start_node, end_node)
+# print(f"Shortest path using Bellman-Ford algorithm from node {start_node} to node {end_node} has cost {bellman_ford_cost} and path {bellman_ford_path}")
 
 # Create a Folium map centered on Singapore
 singapore_latlng = [1.3521, 103.8198]  # Coordinates for Singapore
@@ -112,12 +112,12 @@ m = folium.Map(location=singapore_latlng, zoom_start=12)
 # Extract the coordinates of the nodes in each path
 dijkstra_path_coords = [(G.nodes[node]['y'], G.nodes[node]['x']) for node in dijkstra_path]
 a_star_path_coords = [(G.nodes[node]['y'], G.nodes[node]['x']) for node in a_star_path]
-bellman_ford_path_coords = [(G.nodes[node]['y'], G.nodes[node]['x']) for node in bellman_ford_path]
+# bellman_ford_path_coords = [(G.nodes[node]['y'], G.nodes[node]['x']) for node in bellman_ford_path]
 
 # Add the routes to the Folium map with different colors
 folium.PolyLine(dijkstra_path_coords, color="blue", weight=2.5, opacity=1, tooltip='Dijkstra').add_to(m)
 folium.PolyLine(a_star_path_coords, color="green", weight=2.5, opacity=1, tooltip='A*').add_to(m)
-folium.PolyLine(bellman_ford_path_coords, color="red", weight=2.5, opacity=1, tooltip='Bellman-Ford').add_to(m)
+# folium.PolyLine(bellman_ford_path_coords, color="red", weight=2.5, opacity=1, tooltip='Bellman-Ford').add_to(m)
 
 # Mark the start and end points
 folium.Marker(location=dijkstra_path_coords[0], popup='Start', icon=folium.Icon(color='green')).add_to(m)
